2ce4bdefbb2506b7ef6dadd4f384bf0a52970fc6
catalamarti <marticatalasabate@gmail.com>
2024-11-14 09:50:00 +0000

    ignore nameStyle

--- tests/testthat/test-generatedAtcCohortSet.R
+++ tests/testthat/test-generatedAtcCohortSet.R
@@ -5,7 +5,6 @@ test_that("test same results for ingredient cohorts", {
   expect_warning(cdm <- generateAtcCohortSet(cdm = cdm, name = "test_cohort_1"))
 
   codes <- CodelistGenerator::getATCCodes(cdm)
-  names(codes) <- tolower(names(codes))
   cdm <- generateDrugUtilisationCohortSet(
     cdm = cdm,
     conceptSet = codes,
@@ -13,10 +12,10 @@ test_that("test same results for ingredient cohorts", {
   )
 
   # Collect data from DuckDB tables into R data frames
-  cohort_1_df <- cdm$test_cohort_1 |> dplyr::collect()
-  cohort_2_df <- cdm$test_cohort_2 |> dplyr::collect()
-  attr(cohort_1_df, "cohort_set") <- attr(cohort_1_df, "cohort_set") |>
-    dplyr::select(-"dose_form")
+  cohort_1_df <- cdm$test_cohort_1 |>
+    collectCohort()
+  cohort_2_df <- cdm$test_cohort_2 |>
+    collectCohort()
 
   expect_equal(cohort_1_df, cohort_2_df)
 
